9662e30780a06296f726bb18d482192552f112e6
liaozhenshan <378272027@qq.com>
2024-06-25 10:47:23 +0800

    提交测试git

---
+++ src/main/java/com/lzs/springsecurity/controller/TestGitController.java
@@ -0,0 +1,13 @@
+package com.lzs.springsecurity.controller;
+
+/**
+ * @author liaozhenshan
+ * @version 1.0
+ * @date 2024/6/25 10:46
+ */
+public class TestGitController {
+
+    public static void main(String[] args) {
+        System.out.println("aa");
+    }
+}
